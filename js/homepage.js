d2e9fa93cd29b8fbbbd94e5bdeed4e35e5bfa3d9
Ryan <ryan@ryanjsaunders.com>
2015-09-04 12:57:27 -0500

    RSS links target_blank
    Fix for Issue 1

--- js/homepage.js
+++ js/homepage.js
@@ -3,4 +3,6 @@ jQuery(document).ready(function($){
     var strNewString = $('#secondary').html().replace('An error has occurred, which probably means the feed is down. Try again later.','<em>No new updates at this time.</em>');
     $('#secondary').html(strNewString);
 
+    $('.widget_rss a').attr('target','_blank');
+
 });
